6190c49bfbb6428d11a04de10c05445a3ab89dbe
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-08-30 08:49:02 -0700

    Working on form validation.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -155,11 +155,31 @@ const FormSelect = (props) => {
 
 const Contact = ({ location }) => {
     const [values, setValues] = useState({
-        name: '',
-        email: '',
-        service: '',
-        budget: '',
-        desc: ''
+        name: {
+            value: '',
+            error: '',
+            valid: false
+        },
+        email: {
+            value: '',
+            error: '',
+            valid: false
+        },
+        service: {
+            value: '',
+            error: '',
+            valid: false
+        },
+        budget: {
+            value: '',
+            error: '',
+            valid: false
+        },
+        desc: {
+            value: '',
+            error: '',
+            valid: false
+        }
     })
 
     const budget = [
@@ -182,16 +202,57 @@ const Contact = ({ location }) => {
         service = location.state.service
     }
 
+    const validNameRegex = RegExp(/^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/)
+    const validEmailRegex = RegExp(/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i)
+
     const handleChange = (e) => {
         const {name, value} = e.target
-        validate(name, value)
-        setValues({...values, [name]: value})
+        switch (name) {
+            case 'name':
+                if (validNameRegex.test(value)) {
+                    setValues({...values, [name]: {value: value, error: '', valid: true}})
+                }
+                else {
+                    setValues({...values, [name]: {value, error: 'Name must contain at least 4 characters.', valid: false}})
+                }
+                break;
+            case 'email':
+                if (validEmailRegex.test(value)) {
+                    setValues({...values, [name]: {value: value, error: '', valid: true}})
+                }
+                else {
+                    setValues({...values, [name]: {value, error: 'Please enter a valid email.', valid: false}})
+                }
+                break;
+            case 'service':
+                if (value !== '') {
+                    setValues({...values, [name]: {value: value, error: '', valid: true}})
+                }
+                else {
+                    setValues({...values, [name]: {value, error: 'Please choose a service.', valid: false}})
+                }
+                break;
+            case 'budget':
+                if (value !== '') {
+                    setValues({...values, [name]: {value: value, error: '', valid: true}})
+                }
+                else {
+                    setValues({...values, [name]: {value, error: 'Please choose a budget.', valid: false}})
+                }
+                break;
+            case 'desc':
+                if (value !== '') {
+                    setValues({...values, [name]: {value: value, error: '', valid: true}})
+                }
+                else {
+                    setValues({...values, [name]: {value, error: 'Please tell me a little bit about the job.', valid: false}})
+                }
+                break;
+        }
     }
 
-    const validate = (name, value) => {
-        if (name === "email") {
-
-        }
+    const handleSubmit = (e) => {
+        
     }
 
     return (
@@ -199,28 +260,26 @@ const Contact = ({ location }) => {
             <SEO title="Hire" description="Please drop a line if you would like to contact Josh Drentlaw about a job. He's always looking for work!" />
             <Container padding="1em 0" justifyLg="space-between">
                 <H1>Let me know which service you're interested in and a little bit about the job. I'll be in touch shortly.</H1>
-                <Form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field" noValidate>
+                <Form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field" onSubmit={handleSubmit} noValidate>
                     <input type="hidden" name="form-name" value="contact" />
                     <input type="hidden" name="bot-field" />
-                    <FormInput type="text" name="name" placeholder="Full Name" onChange={handleChange} value={values.name} />
-                    <FormInput type="email" name="email" placeholder="Email" onChange={handleChange} value={values.email} />
+                    <FormInput type="text" name="name" placeholder="Full Name" onChange={handleChange} value={values.name.value} />
+                    <FormInput type="email" name="email" placeholder="Email" onChange={handleChange} value={values.email.value} />
                     <FormSelect
                         name="service"
                         placeholder="Choose a service"
                         list={services}
-                        selection={service}
                         onChange={handleChange}
-                        value={service || values.service}
-                        multiple={true}
+                        value={service || values.service.value}
                     />
                     <FormSelect
                         name="budget"
                         placeholder="Expected Budget"
                         list={budget}
                         onChange={handleChange}
-                        value={values.budget}
+                        value={values.budget.value}
                     />
-                    <textarea name="desc" className="tarea" rows="4" placeholder="Short desc of job..." onChange={handleChange} value={values.desc} required></textarea>
+                    <textarea name="desc" className="tarea" rows="4" placeholder="Short desc of job..." onChange={handleChange} value={values.desc.value} required></textarea>
                     <button className="button" type="submit">Submit</button>
                 </Form>
             </Container>
