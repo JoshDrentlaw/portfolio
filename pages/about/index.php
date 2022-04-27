0bfabdf0c4c4b4c80b6d665bf1128c5d0156085a
Josh Drentlaw <joshdrentlaw@gmail.com>
2022-04-26 19:50:33 -0700

    wip

--- pages/about/index.php
+++ pages/about/index.php
@@ -1,5 +1,13 @@
 <?php include '../../includes/inc.header.php'; ?>
 
-<h1>About</h1>
+<h1>I Make Web Design <span>Simple</span><br>So That You Can Focus On<br><span>What Matters To You</span></h1>
+
+<p>It doesn't matter why you need a website, designing one can be hard and time consuming.</p>
+
+<p>My mission is to deliver a website that makes you feel excited and ready to share what you have to offer.</p>
+
+<p>Please check out my services below and do not hesitate to contact me for a quote.</p>
+
+<div id="services-container"></div>
 
 <?php include '../../includes/inc.footer.php';?>
