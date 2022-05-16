71b5a71b0604cf7b9fa3c4d00b5206d2d5e6c7b7
Josh Drentlaw <joshdrentlaw@gmail.com>
2022-05-15 21:12:38 -0700

    Updated about me text and added image.

--- pages/about/index.php
+++ pages/about/index.php
@@ -2,11 +2,26 @@
 
 <h1>I Make Web Design <span>Simple</span><br>So That You Can Focus On<br><span>What Matters To You</span></h1>
 
-<p>It doesn't matter why you need a website, designing one can be hard and time consuming.</p>
+<div id="about-container">
+    <div id="about-text-container">
+        <p>Your mission to grow your business and connect with your customers, not spend weeks figuring out how to get a
+            website
+            up.</p>
 
-<p>My mission is to deliver a website that makes you feel excited and ready to share what you have to offer.</p>
+        <p>My mission is to deliver an amazing website, so you can spend time on what matters most to you.</p>
 
-<p>Please check out my services below and do not hesitate to contact me for a quote.</p>
+        <p>Please check out my services below and do not hesitate to
+            <a href="/<?= $_ENV['INDEX'] ?>pages/contact/" class="btn btn-primary btn-compact">contact me</a> for a
+            quote.
+        </p>
+    </div>
+    <div id="about-img-container">
+        <figure>
+            <img src="/<?= $_ENV['INDEX'] ?>public/images/cashier.jpg" alt="Cashier" loading="lazy" width="100%">
+            <figcaption>Photo by Christiann Koepke on Unsplash</figcaption>
+        </figure>
+    </div>
+</div>
 
 <div id="services-container"></div>
 
