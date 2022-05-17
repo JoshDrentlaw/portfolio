216286591357d152d7e9d163856ec62e5e4e2598
Josh Drentlaw <joshdrentlaw@gmail.com>
2022-05-16 21:24:42 -0700

    Removed INDEX from site.

--- includes/inc.header.php
+++ includes/inc.header.php
@@ -13,7 +13,7 @@ use Controller\Controller;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/<?= $_ENV['INDEX'] ?>main.css">
+    <link rel="stylesheet" href="/main.css">
     <?php
     $controller = new Controller();
     $controller->includeCss();
@@ -25,19 +25,19 @@ use Controller\Controller;
     <main>
         <nav>
             <div class="nav-endcap" id="head-title-container">
-                <a class="nav-link nav-endcap" id="header-title" href="/<?= $_ENV['INDEX'] ?>">Josh Drentlaw</a>
+                <a class="nav-link nav-endcap" id="header-title" href="/">Josh Drentlaw</a>
             </div>
             <div id="link-container">
                 <a class="nav-link<?= !in_array($controller->pathname, ['about', 'contact', 'projects', 'blog']) ? ' active' : '' ?>"
-                    id="home" href="/<?= $_ENV['INDEX'] ?>">Home</a>
+                    id="home" href="/">Home</a>
                 <a class="nav-link<?= $controller->pathname === 'about' ? ' active' : '' ?>" id="about"
-                    href="/<?= $_ENV['INDEX'] ?>pages/about">About</a>
+                    href="/pages/about">About</a>
                 <a class="nav-link<?= $controller->pathname === 'projects' ? ' active' : '' ?>" id="projects"
-                    href="/<?= $_ENV['INDEX'] ?>pages/projects">Projects</a>
+                    href="/pages/projects">Projects</a>
                 <a class="nav-link<?= $controller->pathname === 'blog' ? ' active' : '' ?>" id="blog"
-                    href="/<?= $_ENV['INDEX'] ?>pages/blog">Blog</a>
+                    href="/pages/blog">Blog</a>
             </div>
             <div class="nav-endcap" id="nav-contact-container">
-                <a class="nav-link" id="nav-contact" href="/<?= $_ENV['INDEX'] ?>pages/contact">Contact</a>
+                <a class="nav-link" id="nav-contact" href="/pages/contact">Contact</a>
             </div>
         </nav>
