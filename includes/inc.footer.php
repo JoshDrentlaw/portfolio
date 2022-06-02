ac448d12ac431e6e06d1e21334d7e225ed595de2
Josh Drentlaw <joshdrentlaw@gmail.com>
2022-06-01 19:08:36 -0700

    Added referral badge to footer.

--- includes/inc.footer.php
+++ includes/inc.footer.php
@@ -1,4 +1,8 @@
 </main>
+<footer>
+    <hr>
+<a href="https://www.digitalocean.com/?refcode=077a23ba0be5&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge"><img src="https://web-platforms.sfo2.digitaloceanspaces.com/WWW/Badge%203.svg" alt="DigitalOcean Referral Badge" /></a>
+</footer>
 <script src="/index.js"></script>
 <?= $controller->includeJs(); ?>
 </body>
